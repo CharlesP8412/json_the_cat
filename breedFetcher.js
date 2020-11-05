a3feaedcfeee3cbb0df7cc578d80582f6c39538e
Charles P <charles.prystupa@gmail.com>
2020-11-05 17:22:47 +0000

    Changed to accept ARGUMENT of breed to find

--- breedFetcher.js
+++ breedFetcher.js
@@ -9,12 +9,20 @@ Search Query
  */
 const request = require('request');
 
+// Take user input w. process ARGV
+const argInput = process.argv.splice(2);
+// Parse out Input, URL & filePath
+const breedToFind = argInput[0];
 
 //Request the siberian data 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=sib`, (error, response, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedToFind.toLowerCase()}`, (error, response, body) => {
+  //Converts Dl'd Body STRING into an Object (array w. object)
+  if (error) {
+    throw new Error(error);
+  }
   const data = JSON.parse(body);
-  console.log(data[0].description)
-  console.log(typeof data)
+  // Print Dl'd desc. of the breed
+  console.log(data[0].description);
 });
 
