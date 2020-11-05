68bc2943e414246620097c883e3f19f768b33016
Charles P <charles.prystupa@gmail.com>
2020-11-05 17:35:30 +0000

    de-linting

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,8 @@
-/* 
+/*
 API
 //   https://api.thecatapi.com/v1/images/search
-Search Query 
+Search Query
 // https://api.thecatapi.com/v1/breeds/search?q=sib
-
-
 
  */
 const request = require('request');
@@ -14,7 +12,7 @@ const argInput = process.argv.splice(2);
 // Parse out Input, URL & filePath
 const breedToFind = argInput[0];
 
-//Request the siberian data 
+//Request the siberian data
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breedToFind.toLowerCase()}`, (error, response, body) => {
   //Converts Dl'd Body STRING into an Object (array w. object)
   if (error) {
@@ -25,6 +23,6 @@ request(`https://api.thecatapi.com/v1/breeds/search?q=${breedToFind.toLowerCase(
       // Print Dl'd desc. of the breed
       console.log(data[0].description);
     }
-    console.log("No data found")
+    console.log("No data found");
   }
 });
